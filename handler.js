0780f247e24486584ac1509d08c8f8030c6ca072
alonsovargas3 <alonsovargas@gmail.com>
2019-03-29 18:29:29 -0700

    Updated SQL query to bring in more specific Zobot information

--- handler.js
+++ handler.js
@@ -21,22 +21,18 @@ app.use(function(req, res, next) {
 app.get('/zobot_status/:exchange/:coin', (req, res) => {
   const exchange = req.params.exchange
   const coin = req.params.coin.toUpperCase();
-  console.log("coin="+coin);
   let pair;
   if(coin=='BTC'){
     pair = coin+"/USDT";
   } else{
     pair = coin+"/BTC";
   }
-  console.log(exchange + " " + pair);
-  const query = `SELECT ID,pair,exchange,coinStatus,price,timeFrame FROM coinData WHERE pair = '${pair}' AND exchange = '${exchange}' ORDER BY timeFrame`;
-  console.log(query);
-  pool.query(query, (err, results, fields) => {
+  const query = 'SELECT coinData.*,TRUNCATE((((coinData.price - coinData.foundPrice) / coinData.foundPrice) * 100),2) AS pctg,DATE_FORMAT(coinData.foundTime, '%Y-%m-%d %H:%i') AS foundTime,coins.coin FROM coinData INNER JOIN coins ON SUBSTRING_INDEX(coinData.pair, '/', 1) = coins.ticker WHERE pair = ? AND exchange = ? ORDER BY timeFrame'
+  pool.query(query, [pair,exchange] (err, results, fields) => {
     if (err) {
       const response = { data: null, message: err.message, }
       res.send(response)
     }
-    console.log(results);
     const zobot_results_1d = results[0]
     const zobot_results_1h = results[1]
     const response = {
