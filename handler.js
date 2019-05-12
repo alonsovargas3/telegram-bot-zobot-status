e88e8d53703819be68d431d54a498e094e854471
alonsovargas3 <alonsovargas@gmail.com>
2019-05-11 21:40:27 -0700

    Updated response to exclude Details link

--- handler.js
+++ handler.js
@@ -22,23 +22,68 @@ app.get('/zobot_status/:exchange/:coin', (req, res) => {
   const exchange = req.params.exchange
   const coin = req.params.coin.toUpperCase();
   let pair;
+
   if(coin=='BTC'){
     pair = coin+"/USDT";
   } else{
     pair = coin+"/BTC";
   }
-  const query = 'SELECT coinData.*,TRUNCATE((((coinData.price - coinData.foundPrice) / coinData.foundPrice) * 100),2) AS pctg,DATE_FORMAT(coinData.foundTime, '%Y-%m-%d %H:%i') AS foundTime,coins.coin FROM coinData INNER JOIN coins ON SUBSTRING_INDEX(coinData.pair, '/', 1) = coins.ticker WHERE pair = ? AND exchange = ? ORDER BY timeFrame'
-  pool.query(query, [pair,exchange] (err, results, fields) => {
+
+  const query = "SELECT coinData.*,TRUNCATE((((coinData.price - coinData.foundPrice) / coinData.foundPrice) * 100),2) AS pctg,DATE_FORMAT(coinData.foundTime, '%Y-%m-%d %H:%i') AS foundTime,coins.coin FROM coinData INNER JOIN coins ON SUBSTRING_INDEX(coinData.pair, '/', 1) = coins.ticker WHERE pair = ? AND exchange = ? ORDER BY timeFrame"
+  pool.query(query, [pair,exchange], (err, results, fields) => {
     if (err) {
       const response = { data: null, message: err.message, }
       res.send(response)
     }
+
     const zobot_results_1d = results[0]
     const zobot_results_1h = results[1]
+
+    let pre_symbol_1d;
+    let pre_symbol_1h;
+
+    let pre_price = "";
+    if(coin=='BTC'){
+      pre_price = "$";
+    }
+
+    let pctg_1d = zobot_results_1d.pctg;
+    let pctg_1h = zobot_results_1h.pctg;
+
+    if(pctg_1d>0){
+      pre_symbol_1d = "▲";
+    } else if (pctg_1d<0){
+      pre_symbol_1d = "▼";
+    } else if (pctg_1d == 0){
+      pre_symbol_1d = "=";
+    }
+
+    if(pctg_1h>0){
+      pre_symbol_1h = "▲";
+    } else if (pctg_1h<0){
+      pre_symbol_1h = "▼";
+    } else if (pctg_1h == 0){
+      pre_symbol_1h = "=";
+    }
+
     const response = {
       data: results,
-      message: pair + " status: \n 1d: " + zobot_results_1d.coinStatus + " \n 1hr: " + zobot_results_1h.coinStatus,
+      message: "<b>" + pair + "</b>" +
+      "\n 1d: " + zobot_results_1d.coinStatus +
+      "\n Price: " + pre_price + zobot_results_1d.price +
+      "\n Found price: " + pre_price + zobot_results_1d.foundPrice +
+      "\n Found time: " + zobot_results_1d.foundTime +
+      "\n Change: " + pre_symbol_1d +  " " + zobot_results_1d.pctg + "%" +
+      //"\n Details: <a href='https://app.buenavistacryptoclub.com/zobot_coin_detail.html?c=" + coin + "&t=1d&exchange=" + exchange + "'>Click here</a>" +
+
+      "\n \n 1hr: " + zobot_results_1h.coinStatus +
+      "\n price: " + pre_price + zobot_results_1h.price +
+      "\n found price: " + pre_price + zobot_results_1h.foundPrice +
+      "\n found time: " + zobot_results_1h.foundTime +
+      "\n change: " + pre_symbol_1h +  " " + zobot_results_1h.pctg + "%",
+      //"\n Details: <a href='https://app.buenavistacryptoclub.com/zobot_coin_detail.html?c="+ coin +"&t=1hr&exchange=" + exchange + "'>Click here</a>",
     }
+
     res.status(200).send(response)
   })
 })
